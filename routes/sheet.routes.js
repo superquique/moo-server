03bb78036272f08d8ca05f384675fa87e6f3a583
superquique <enriqueveintidos@gmail.com>
2026-04-13 10:53:23 -0600

    feat: implement sheet routes

---
+++ routes/sheet.routes.js
@@ -0,0 +1,97 @@
+const router = require("express").Router();
+const Sheet = require("../models/Sheet.model");
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
+const { isOwner } = require("../middleware/owner.middleware.js");
+
+// POST / Create a sheet
+router.post("/", isAuthenticated, (req, res, next) => {
+    const userId = req.payload._id;
+    const newSheet = req.body;
+
+    newSheet.user = userId;
+
+    Sheet.create(newSheet)
+    .then((sheetFromDb) => {
+      console.log("Created new sheet", sheetFromDb);
+      res.status(201).json(sheetFromDb);
+    })
+    .catch((error) => {
+      next(error);
+    })
+})
+
+// PUT / Update a sheet
+router.put("/:id", isAuthenticated, isOwner(Sheet), (req, res, next) => {
+    const { id } = req.params;
+    const newDetails = req.body;
+
+    console.log(newDetails);
+    
+    Sheet.findByIdAndUpdate(id, newDetails, {returnDocument: 'after'})
+    .then((sheetFromDb) => {
+      console.log("Updated sheet", sheetFromDb);
+      res.status(200).json(sheetFromDb);
+    })
+    .catch((error) => {
+      next(error);
+    })
+})
+
+// DELETE / Delete a sheet
+router.delete("/:id", isAuthenticated, isOwner(Sheet), (req, res, next) => {
+    const { id } = req.params;
+
+    Sheet.findByIdAndDelete(id)
+    .then((result) => {
+      console.log("Deleted sheet", result);
+      res.status(204).send();
+    })
+    .catch((error) => {
+      next(error);
+    })
+})
+
+// GET /:id - Get a sheet by id
+router.get("/:id", isAuthenticated, isOwner(Sheet), (req, res, next) => {
+    console.log(`req.payload`, req.payload);
+
+    try {
+        res.status(200).json(req.resource);
+    } catch (error) {
+        next(error);
+    }
+    
+});
+
+// GET / - Get a list of sheets
+router.get("/", isAuthenticated, (req, res, next) => {
+    console.log(`req.payload`, req.payload);
+    const userId = req.payload._id;
+
+    Sheet.find({user: userId})
+    .then((sheets) => {
+      console.log("Found sheets", sheets);
+      res.status(200).json(sheets);
+    })
+    .catch((error) => {
+      next(error);
+    })
+})
+
+// GET / - Get a list of sheets from notebook
+router.get("/notebook/:notebookId", isAuthenticated, (req, res, next) => {
+    console.log(`req.payload`, req.payload);
+    const userId = req.payload._id;
+    const { notebookId } = req.params;
+
+    Sheet.find({user: userId, notebook: notebookId})
+    .then((sheets) => {
+      console.log("Found sheets", sheets);
+      res.status(200).json(sheets);
+    })
+    .catch((error) => {
+      next(error);
+    })
+})
+
+module.exports = router;
